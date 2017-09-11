cf4d593becc3857de6c1ed6efca0dfc21eea5bcc
Emil Edberg <edem16@nti.umea>
2017-09-11 11:01:17 +0200

    uppg 1.9, 1.1, 1.2

--- Kapitel1.java
+++ Kapitel1.java
@@ -1,12 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
-public class Kapitel1{
-	public static void main(String[] args){
-		JOptionPane.showMessageDialog(null, "Du k" + '\u00F6' + "r " +System.getProperty("os.name") + " idag!");
-		Toolkit.getDefaultToolkit() .beep();
-		JOptionPane.showMessageDialog(null, "0703902222");
-		Toolkit.getDefaultToolkit() .beep();
-
+	public class Kapitel1{
+		public static void main(String[] args){
+			//JOptionPane.showMessageDialog(null, "Du k" + '\u00F6' + "r " +System.getProperty("os.name") + " idag!");
+			//Toolkit.getDefaultToolkit() .beep();
+			//JOptionPane.showMessageDialog(null, "0703902222");
+			//Toolkit.getDefaultToolkit() .beep();
+			//JOptionPane.showMessageDialog(null, "Emil Edberg \nVB 29 \n0703902222");
+			//JOptionPane.showMessageDialog(null, "Datum och Tid: \n" + Calendar.getInstance () .getTime() .toString ());
+			JOptionPane.showMessageDialog(null, "Info", "Information", JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Varning", "Varning", JOptionPane.WARNING_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Fel","Felmeddelande", JOptionPane.ERROR_MESSAGE);
 	}
 }
