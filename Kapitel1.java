784d9ed6735d81908c03d4e7752fda2ff6080ca5
Emil Edberg <edem16@nti.umea>
2017-09-11 10:27:22 +0200

    uppg 1.6-1.8

---
+++ Kapitel1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Kapitel1{
+	public static void main(String[] args){
+		JOptionPane.showMessageDialog(null, "Du k" + '\u00F6' + "r " +System.getProperty("os.name") + " idag!");
+		Toolkit.getDefaultToolkit() .beep();
+		JOptionPane.showMessageDialog(null, "0703902222");
+		Toolkit.getDefaultToolkit() .beep();
+
+	}
+}
